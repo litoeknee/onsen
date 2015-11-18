bc808840c549f85bb6b4840e29648d69aff96d6d
toeknee <litoeknee@gmail.com>
2015-11-18 13:39:57 +0800

    2nd commit

---
+++ src/org/toeknee/signUp/servlet/CoreServlet.java
@@ -0,0 +1,48 @@
+package org.toeknee.signUp.servlet;
+
+/**
+ * Created by toeknee on 11/18/15.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.toeknee.signUp.util.SignInUtil;
+
+public class CoreServlet extends HttpServlet {
+    private static final long serialVersionUID = 4440739483644821986L;
+
+    /**
+     * request check: to check if the request is from weChat server
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // encoded signature from weChat
+        String signature = request.getParameter("signature");
+        // timestamp from weChat
+        String timestamp = request.getParameter("timestamp");
+        // a random number from weChat
+        String nonce = request.getParameter("nonce");
+        // a random string from weChat
+        String echostr = request.getParameter("echostr");
+
+        PrintWriter out = response.getWriter();
+        // if ciphertext equals to signature then print and return the string
+        if (SignInUtil.checkSignature(signature, timestamp, nonce)) {
+            out.print(echostr);
+        }
+        out.close();
+        out = null;
+    }
+
+    /**
+     * ?????????????????????
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO ????????????????
+    }
+}
